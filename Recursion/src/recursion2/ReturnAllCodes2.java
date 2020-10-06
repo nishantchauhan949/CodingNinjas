f156b093d647725aafc67a82ea9396f17b0837b1
Nishant <nishantchauhan949@hotmail.com>
2020-10-06 17:24:18 +0530

    ReturnAllCodes -> ReturnAllCodes2

--- Recursion/src/recursion2/ReturnAllCodes2.java
+++ Recursion/src/recursion2/ReturnAllCodes2.java
@@ -2,11 +2,11 @@ package recursion2;
 
 import java.util.Scanner;
 
-public class ReturnAllCodes {
+public class ReturnAllCodes2 {
 
-    public static  String[] getCode(String input){
-
-    }
+    // public static  String[] getCode(String input){
+    //     return null;
+    // }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
